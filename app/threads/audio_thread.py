a85c2f7ac00ca4a746ea5260d04612d45d625408
0eoeo <1707.0eoeo@gmail.com>
2025-05-03 16:09:16 +0300

    ыв

--- app/threads/audio_thread.py
+++ app/threads/audio_thread.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+
+import pyaudio
 import time
 
 
@@ -9,7 +11,8 @@ class AudioThread(threading.Thread):
         self.server_ip = server_ip
         self.server_port = server_port
         self.client_socket = None
-        self.running = True  # Изначально поток будет работать
+        self.running = False
+        self.p = pyaudio.PyAudio()
 
     def run(self):
         """Запуск работы потока."""
@@ -19,35 +22,48 @@ class AudioThread(threading.Thread):
             self.client_socket.connect((self.server_ip, self.server_port))
             print("Audio client connected to server")
 
+            # Параметры записи (16 бит, моно, частота 44100)
+            self.stream = self.p.open(format=pyaudio.paInt16,
+                                      channels=1,
+                                      rate=44100,
+                                      input=True,
+                                      frames_per_buffer=1024)
+
             while self.running:
-                # Здесь вставьте код для записи и отправки аудио
-                data = b"Example audio chunk"  # Пример данных
+                # Чтение данных из микрофона
+                data = self.stream.read(1024)
+
+                # Отправка аудио данных через сокет
                 self.send_audio_data(data)
-                # Можно добавить паузу, чтобы не перегружать процесс
-                time.sleep(0.5)
+
+                # Добавим паузу, чтобы не перегружать процесс
+                time.sleep(0.05)
 
         except socket.error as e:
             print(f"Audio thread socket error: {e}")
+        except Exception as e:
+            print(f"Ошибка: {e}")
         finally:
             self.close_socket()
 
     def send_audio_data(self, data):
-        """Отправка аудио данных на сервер."""
+        """Отправка аудио данных через сокет."""
         try:
             self.client_socket.sendall(data)
+            print("Sent audio data chunk")
         except socket.error as e:
             print(f"Error sending audio data: {e}")
-            self.close_socket()
 
     def close_socket(self):
         """Закрытие сокета при завершении работы потока."""
         if self.client_socket:
             self.client_socket.close()
             self.client_socket = None
-            print("Socket closed.")
+        if hasattr(self, "stream"):
+            self.stream.stop_stream()
+            self.stream.close()
+        self.p.terminate()
 
     def stop(self):
         """Остановка потока."""
         self.running = False
-        self.close_socket()
-        print("Audio thread stopped.")
